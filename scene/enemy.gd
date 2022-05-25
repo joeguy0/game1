b3c5a7627769faa0cfd2ffde4e9ae8839d561667
Flanders <flanders.lorton@gmail.com>
2022-05-24 22:03:24 -0700

    Enemy flash red when hit

--- scene/enemy.gd
+++ scene/enemy.gd
@@ -1,11 +1,9 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 const GRAV = 50
+
+var hitTimer
+
 #var rng
 var velocity
 var hP = 10
@@ -13,9 +11,10 @@ onready var player = get_tree().get_nodes_in_group("player")[0]
 
 func _ready():
 	velocity = Vector2.ZERO
+	hitTimer = get_node("atk_hit_timeout")
+	hitTimer.connect("timeout",self,"on_atk_hit_timeout")
+	hitTimer.set_wait_time(.05)
 	#add_collision_exception_with(player)
-	#rng = RandomNumberGenerator.new()
-	#rng.randomize()
 
 func _physics_process(delta):
 	#var num = (rng.randi_range(0, 4) - 2) * 100
@@ -33,7 +32,6 @@ func _physics_process(delta):
 
 	velocity.x = clamp(velocity.x, -1000, 1000)
 	velocity = move_and_slide(velocity,Vector2.UP)
-
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,5 +42,11 @@ func _physics_process(delta):
 func _on_eneHurtBox_area_entered(area):
 	if area.get_name() == "bulletHitBox":
 		hP = hP - 1 
+		$AnimatedSprite.set_modulate(Color ("ff557d"))
+		hitTimer.start()
 	if hP < 1:
 		queue_free()
+
+func on_atk_hit_timeout():
+	$AnimatedSprite.set_modulate(Color ("ffffff"))
+	hitTimer.stop()
